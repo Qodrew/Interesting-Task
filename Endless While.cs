cdaa97749f8c295afaf5e47c8bf75cc6b9c1922d
Qodrew <120235371+Qodrew@users.noreply.github.com>
2022-12-26 20:05:24 +0300

    Update Endless While.cs

--- Endless While.cs
+++ Endless While.cs
@@ -8,3 +8,17 @@ if (double.TryParse(s, out n)) // преобразование в double
 { sum += n; // суммирование чисел
 Console.WriteLine("сумма = " + sum); }
 }
+
+// Across do .. while
+double n = 0, sum = 0; string s = "";
+do
+{
+Console.Write("Введите число: ");
+s = Console.ReadLine(); // ввод
+if (double.TryParse(s, out n)) // преобразование в double
+{ sum += n; // суммирование чисел
+Console.WriteLine("сумма = " + sum); }
+
+// повторяем ввод до тех пор, пока не введен символ “Q”
+
+} while (s!="Q"); // выход из цикла по условию
