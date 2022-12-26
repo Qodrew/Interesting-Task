151a6cfb3d976f946f2b56e7a8177eb28b1f99c1
Qodrew <120235371+Qodrew@users.noreply.github.com>
2022-12-26 11:44:32 +0300

    На входе число

---
+++ Таблица кубов.cs
@@ -0,0 +1,8 @@
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число: ");
+int incomNum = Convert.ToInt32(Console.ReadLine());
+for(int i = 1; i <= incomNum; i++)
+{
+    Console.Write($"{Math.Pow(i, 3)} ");
+}
