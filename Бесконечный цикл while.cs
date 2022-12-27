96d7384c73aaebfadb5d3300064b03fc69188d7b
Qodrew <120235371+Qodrew@users.noreply.github.com>
2022-12-27 13:59:23 +0300

    Create Бесконечный цикл while.cs

---
+++ Бесконечный цикл while.cs
@@ -0,0 +1,14 @@
+// Повторяются вычисления и вывод площади круга по вводимому радиусу r до тех пор, пока не введена буква z или Z .
+// Беспконечный чикл while!
+
+double square = 0, num = 0, pi = 3.14;  string radCircle = "";
+
+while (true) {
+    Console.Write("Введите радиус круга: ");
+    radCircle = Console.ReadLine();
+    if (radCircle == "Stop" || radCircle == "s") break;
+    if (double.TryParse(radCircle, out num)) {
+        square = pi*num*num;
+        Console.WriteLine("Площадь равна = " + string.Format("{0:0.000}", square));
+    }
+}
